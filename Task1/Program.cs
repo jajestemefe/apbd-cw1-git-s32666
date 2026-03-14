cd98b98cd56f9b0e2de252df83a4099687aad892
Efe Ozarslan <s32666@pjwstk.edu.pl>
2026-03-14 16:20:51 +0100

    area calculator

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
+int x  = 3;
+int y = 4;
+Console.WriteLine("Calculating area of rectangle with edges " +  x " and " + y " ...");
+int area = x * y;
+Console.WriteLine("Area: " +  area);
