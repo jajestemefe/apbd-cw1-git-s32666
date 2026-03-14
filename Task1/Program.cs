7016fe0b899669c13d1b1727f94316dcb6f00fc4
Efe Ozarslan <s32666@pjwstk.edu.pl>
2026-03-14 17:17:35 +0100

    messy code rewritten in CalculateAreaRec method

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("Hello, World!");
-int x  = 3;
-int y = 4;
-Console.WriteLine("Calculating area of rectangle with edges " +  x " and " + y " ...");
-int area = x * y;
-Console.WriteLine("Area: " +  area);
+
+int CalculateAreaRec(int a, int b)
+{
+    return a*b;
+}
 
 int CalculateAverage(int[] values)
 {
