21edee7407fb8c948057da10e34877bde5e715b1
Efe Ozarslan <s32666@pjwstk.edu.pl>
2026-03-14 16:33:40 +0100

    method CalculateAverage added

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,8 @@ int y = 4;
 Console.WriteLine("Calculating area of rectangle with edges " +  x " and " + y " ...");
 int area = x * y;
 Console.WriteLine("Area: " +  area);
+
+CalculateAverage(int[] values)
+{
+
+}
